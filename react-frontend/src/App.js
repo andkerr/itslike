0cb119be6345656e00b362cfd55279b5a1479ceb
Andrew Kerr <kerrand@protonmail.com>
2021-10-15 23:06:22 -0400

    remove uneeded React files

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -14,10 +13,6 @@ function App() {
     return (
         <div className="App">
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-            Edit <code>src/App.js</code> and save to reload.
-            </p>
             {placeholder}
         </header>
         </div>
